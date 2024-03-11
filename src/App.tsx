a06dfb2a12359799605904572f9f4efab005f829
Elvin Shahsuvarli <shahsuvarli.elvin@gmail.com>
2024-03-11 21:44:26 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+      </a>
+    </div>
+  );
+}
+
+export default App;
