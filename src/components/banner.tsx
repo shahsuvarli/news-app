dd85b076208de996de8982baf952211affdb087c
Elvin Shahsuvarli <shahsuvarli.elvin@gmail.com>
2024-03-12 13:59:00 +0100

    inital routing added (#14)

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom";
 
 const Banner = () => {
   return (
-    <div className="w-full h-14 bg-slate-600 text-slate-100 p-2">Banner</div>
-  )
-}
+    <div className="w-full h-14 bg-slate-600 text-slate-100 p-2">
+      <Link to={"/"}>NGT - Neutral Global Timely</Link>
+    </div>
+  );
+};
 
-export default Banner
+export default Banner;
--- src/components/sources.tsx
+++ src/components/sources.tsx
@@ -1,22 +1,22 @@
+import { Link } from "react-router-dom";
 import sources from "../assets/sources.json";
 
 const Sources = () => {
   return (
     <div className="flex justify-center flex-col items-center gap-4">
       <p>Sources</p>
-      <div className="flex flex-row flex-wrap gap-4">
+      <div className="flex flex-row flex-wrap gap-4 justify-center">
         {sources.map((item) => (
-          <div
-            key={item.id}
-            className="border-slate-600 border-solid border-2 w-60 h-60 relative flex flex-col justify-evenly items-center hover:cursor-pointer"
-          >
-            <img
-              src={item.image}
-              alt={item.name}
-              className="w-32 h-32 object-contain"
-            />
-            <p className="text-slate-600 text-xl">{item.name}</p>
-          </div>
+          <Link to={`/news-source/${item.link}`} key={item.id}>
+            <div className="border-slate-600 border-solid border-2 w-60 h-60 relative flex flex-col justify-evenly items-center hover:cursor-pointer">
+              <img
+                src={item.image}
+                alt={item.name}
+                className="w-32 h-32 object-contain"
+              />
+              <p className="text-slate-600 text-xl">{item.name}</p>
+            </div>
+          </Link>
         ))}
       </div>
     </div>
