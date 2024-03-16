34ce86d93f06a7ae78b8512c2a0fdd52d9c541c8
Elvin Shahsuvarli <shahsuvarli.elvin@gmail.com>
2024-03-16 04:39:13 +0100

    Navbar deisgn updated (#19)

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,35 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { FaRegNewspaper } from "react-icons/fa";
 
 const Header = () => {
+  const { pathname } = useLocation();
   return (
-    <div className="w-full h-14 bg-slate-600 text-slate-100 p-2">
-      <Link to={"/"}>NGT - Neutral Global Timely</Link>
+    <div className="text-slate-700 w-full h-20 bg-slate-200 p-2 flex flex-row gap-4 items-center justify-between px-8">
+      <span className="flex flex-row gap-10 text-slate-800 items-center">
+        <span className="flex flex-row gap-6 items-center">
+          <FaRegNewspaper size={34} />
+          <Link to={"/"}>
+            <p className="text-xl font-medium">Daily News</p>
+          </Link>
+        </span>
+      </span>
+      <span className="flex flex-row gap-6 text-xl text-slate-600">
+        <Link to={"/"} id="search" className={``}>
+          Home
+        </Link>
+        <Link to={"/search"} id="search">
+          Search
+        </Link>
+      </span>
+      <span className="flex flex-row gap-4 items-center">
+        <img
+          src="https://avatars.githubusercontent.com/u/46631807?v=4"
+          alt="photo"
+          width={54}
+          className="rounded-full border-2 border-solid border-slate-700"
+        />
+        <p className="text-xl">Elvin Shahsuvarli</p>
+      </span>
     </div>
   );
 };
