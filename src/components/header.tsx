f7d55fadffcef637f510cd1624b08617f67f5cbc
Elvin Shahsuvarli <shahsuvarli.elvin@gmail.com>
2024-03-16 08:03:54 +0100

    Revert "Revert "Mobile responsiveness provided (#23)" (#24)" (#26)
    This reverts commit c64219c3ffccba5c4a9023ff9df55716a4186866.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,20 +3,20 @@ import { FaRegNewspaper } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <div className="text-slate-700 w-full h-20 bg-slate-200 p-2 flex flex-row gap-4 items-center justify-between px-8">
-      <span className="flex flex-row gap-10 text-slate-800 items-center">
-        <span className="flex flex-row gap-6 items-center">
+    <div className="text-slate-700 w-full h-20 bg-slate-200 p-2 flex flex-row items-center justify-between px-8">
+      <span className="flex flex-row text-slate-800 items-center">
+        <span className="flex flex-row gap-2 items-center">
           <FaRegNewspaper size={34} />
           <Link to={"/"}>
-            <p className="text-xl font-medium">Daily News</p>
+            <p className="font-medium hidden text-xl sm:block">Daily News</p>
           </Link>
         </span>
       </span>
       <span className="flex flex-row gap-6 text-xl text-slate-600">
-        <Link to={"/"} id="search" className={``}>
+        <Link to={"/"} id="home" className='sm:text-xl text-base'>
           Home
         </Link>
-        <Link to={"/search"} id="search">
+        <Link to={"/search"} id="search" className='sm:text-xl text-base'>
           Search
         </Link>
       </span>
@@ -25,9 +25,9 @@ const Header = () => {
           src="https://avatars.githubusercontent.com/u/46631807?v=4"
           alt="photo"
           width={54}
-          className="rounded-full border-2 border-solid border-slate-700"
+          className="rounded-full border-2 border-solid border-slate-700 min-w-10 min-h-10"
         />
-        <p className="text-xl">Elvin Shahsuvarli</p>
+        <p className="text-xl hidden sm:block">Elvin Shahsuvarli</p>
       </span>
     </div>
   );
